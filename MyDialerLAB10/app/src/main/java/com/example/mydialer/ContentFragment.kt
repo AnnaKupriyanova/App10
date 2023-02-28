428fee00da324f7bf77b2d2d76c154c59f288fa3
mialenna <94033868+AnnaKupriyanova@users.noreply.github.com>
2023-02-28 14:26:38 +0700

    Add files via upload

--- MyDialerLAB10/app/src/main/java/com/example/mydialer/ContentFragment.kt
+++ MyDialerLAB10/app/src/main/java/com/example/mydialer/ContentFragment.kt
@@ -1,14 +1,22 @@
 package com.example.mydialer
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.*
+import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 import java.util.concurrent.Callable
@@ -18,50 +26,76 @@ import java.util.concurrent.Executors
 const val url = "https://drive.google.com/u/0/uc?id=1-KO-9GA3NzSgIc1dkAsNm8Dqw0fuPxcR&export=download"
 
 class ContentFragment : Fragment() {
-    private var adapter = Adapter()
-    private val contactList = contactsRequest()
+    private val adapter: Adapter = Adapter { number ->
+        val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$number"))
+        startActivity(callIntent)
+    }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_content, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
 
-        adapter.submitList(contactList)
-        val recyclerView: RecyclerView = view.findViewById(R.id.rView)
-        recyclerView.layoutManager = LinearLayoutManager(view.context)
-        recyclerView.adapter = adapter
-    }
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://drive.google.com/u/0/uc?id=1-KO-9GA3NzSgIc1dkAsNm8Dqw0fuPxcR&export=download")
+            .build()
 
-    private fun contactsRequest(): ArrayList<Contact> {
-        val myService: ExecutorService = Executors.newFixedThreadPool(2)
-        val contactList = myService.submit(Callable {
-            val connection = URL(url).openConnection() as HttpURLConnection
-            val jsonData = connection.inputStream.bufferedReader().readText()
-            connection.disconnect()
-            Gson().fromJson(jsonData, Array<Contact>::class.java).toList() as ArrayList<Contact>
-        })
-        return contactList.get()
-    }
+        client.newCall(request).enqueue(object : Callback {
 
-    fun search(request: CharSequence)
-    {
-        val searchList = arrayListOf<Contact>()
-        if(request.isNotBlank()){
-            for(contact in contactList){
-                if ((contact.name.contains(request)) or
-                    (contact.phone.contains(request)) or
-                    (contact.type.contains(request))){
-                    searchList.add(contact)
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+
+                    val recyclerView = view.findViewById<RecyclerView>(R.id.rView)
+                    val searchEditText = activity?.findViewById<EditText>(R.id.et_search)
+
+                    val json = response.body()?.string()
+                    val gson = GsonBuilder().create()
+                    var phones = gson.fromJson(json, Array<Contact>::class.java)
+                        .toList() as ArrayList<Contact>
+
+                    activity?.runOnUiThread{
+                        recyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+                        recyclerView.adapter = adapter
+
+                        adapter.submitList(phones)
+                        searchEditText?.addTextChangedListener {
+                            val filteredPhones = phones.filter { item ->
+                                item.phone.lowercase().contains(it.toString().lowercase()) ||
+                                        item.name.lowercase().contains(it.toString().lowercase()) ||
+                                        item.type.lowercase().contains(it.toString().lowercase())
+                            }
+                            adapter.submitList(filteredPhones)
+
+                            val filterState = activity?.getSharedPreferences("app_preferences",
+                                AppCompatActivity.MODE_PRIVATE
+                            )?.edit()
+                            filterState?.putString("SEARCH_FILTER", it.toString())?.apply()
+                        }
+
+                        val lastFilterText = activity?.getSharedPreferences("app_preferences",
+                            AppCompatActivity.MODE_PRIVATE
+                        )?.getString("SEARCH_FILTER", "")
+                        searchEditText?.append(lastFilterText)
+                    }
                 }
             }
-        }
-        else {
-            searchList.addAll(contactList)
-        }
-        adapter.submitList(searchList)
+        })
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ContentFragment()
     }
 }
--- MyDialerLAB10/app/src/main/java/com/example/mydialer/MainActivity.kt
+++ MyDialerLAB10/app/src/main/java/com/example/mydialer/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mydialer
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -19,30 +20,12 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("CommitTransaction")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val contentFragment = ContentFragment()
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container_view,
-                    contentFragment, null)
-                .commit()
-        }
-
-        val searchString = findViewById<EditText>(R.id.et_search)
-        searchString.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(request: Editable) {
-
-            }
-            override fun beforeTextChanged(request: CharSequence, start: Int,
-                                           count: Int, after: Int) {
-            }
-            override fun onTextChanged(request: CharSequence, start: Int,
-                                       before: Int, count: Int) {
-                contentFragment.search(request)
-            }
-        })
+        supportFragmentManager.beginTransaction().
+        replace(R.id.fragment_container_view, ContentFragment.newInstance()).commit()
     }
 }
